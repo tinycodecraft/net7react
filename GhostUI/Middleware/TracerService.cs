ed65e349e08af9e0cdeb90830acb01f60620a33f
tinycodecraft <raymond.cw.yau@gmail.com>
2023-08-15 17:51:14 +0800

    add environment checking

--- GhostUI/Middleware/TracerService.cs
+++ GhostUI/Middleware/TracerService.cs
@@ -8,13 +8,15 @@ namespace GhostUI.Middleware
     public class TracerService : BackgroundService
     {
         private readonly ILogger _logger;
-        public TracerService(ILogger<TracerService> logger) { 
+        private readonly IHostEnvironment _env;
+        public TracerService(ILogger<TracerService> logger,IHostEnvironment env) { 
             _logger = logger;
+            _env = env;
         }
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            this._logger.LogInformation(new EventId(2000, "Trace"), "Start {ClassName}.{MethodName}...",
-                      nameof(TracerService), nameof(this.ExecuteAsync));
+            this._logger.LogInformation(new EventId(2000, "Trace"), "Start {ClassName}.{MethodName}... on Environment {ASPNETCORE}",
+                      nameof(TracerService), nameof(this.ExecuteAsync), _env.IsProduction() ? "Production": "Non-Production");
 
             var sensorInput = new { Latitude = 25, Longitude = 134 };
             this._logger.LogInformation("Processing {@SensorInput}", sensorInput);
